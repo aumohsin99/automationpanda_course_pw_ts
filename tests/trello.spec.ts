f46698a94c7cf040a2b971e263aad0e1ceee14b9
Ata Ul Mohsin <ataulmohsin1999@gmail.com>
2024-07-15 22:50:36 +0500

    fixtures added

--- tests/trello.spec.ts
+++ tests/trello.spec.ts
@@ -1,11 +1,8 @@
-import { test, expect } from "@playwright/test";
-import { GetStartedPage } from "./pages/get-started";
-import { BoardPage } from "./pages/board";
-import { MyBoardsPage } from "./pages/my-boards";
-
 import exp from "constants";
 
 import { request } from "http";
+
+import { test, expect } from "./fixtures/trello-test";
 
 test.beforeAll(async ({ request }) => {
   // I don't know what's going on here.
@@ -13,11 +10,17 @@ test.beforeAll(async ({ request }) => {
   await request.post("http://localhost:3000/api/reset");
 });
 
-test("Create a new board with a list and cards", async ({ page }) => {
+// test('Create a new board with a list and cards', async ({ page }) => {
+
+test("Create a new board with a list and cards", async ({
+  getStartedPage,
+  boardPage,
+  myBoardsPage,
+}) => {
   // POM Implementation
-  const getStartedPage = new GetStartedPage(page);
-  const boardPage = new BoardPage(page);
-  const myBoardsPage = new MyBoardsPage(page);
+  //   const getStartedPage = new GetStartedPage(page);
+  //   const boardPage = new BoardPage(page);
+  //   const myBoardsPage = new MyBoardsPage(page);
 
   await getStartedPage.load();
   await getStartedPage.createFirstBoard("Board1");
@@ -31,8 +34,6 @@ test("Create a new board with a list and cards", async ({ page }) => {
 
   await boardPage.addCardToList(0, "Task 3");
 
-  //await boardPage.addCardToList(0, "Task 1");
-
   //confirmation of tasks on page
   await expect(boardPage.cardsLocator).toHaveText([
     "Task 1",
@@ -43,5 +44,4 @@ test("Create a new board with a list and cards", async ({ page }) => {
   await boardPage.goHome();
 
   await myBoardsPage.expectLoaded(["Board1"]);
-  //await expect(myBoardsPage.expectLoaded(["Board1"]));
 });
